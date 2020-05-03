e21b3400617c271f5ebe40dc950741b8d3792dcc
Mihai Dascal <mihai.dascal@ellation.com>
2020-05-03 21:21:48 +0300

    v0.0.6

--- src/__tests__/build-client.test.ts
+++ src/__tests__/build-client.test.ts
@@ -1,10 +1,8 @@
-import {GenerateSDK} from "../build-client";
+import { GenerateSDK } from '../build-client';
 
 test('Wrong options input', () => {
-    expect.assertions(1);
-    return GenerateSDK({
-        outPath: '/'
-    }).catch(e =>
-        expect(e).toEqual(new Error('at least one of spec or specURL should be provided'))
-    );
+  expect.assertions(1);
+  return GenerateSDK({
+    outPath: '/',
+  }).catch((e) => expect(e).toEqual(new Error('at least one of spec or specURL should be provided')));
 });
